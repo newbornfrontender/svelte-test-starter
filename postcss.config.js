f6a624acfa920f9fa093947583c1b26ba54c606b
Infant Frontender <newbornfrontender@gmail.com>
2019-07-18 18:59:50 +0300

    feat: add postcss-use

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const { browserslist: browsers } = require('./package.json');
 
 module.exports = ({ production }) => ({
   plugins: {
+    'postcss-use': true,
     'postcss-preset-env': {
       stage: 0,
       autoprefixer: production,
