6a2e04ca970242b4a1c3780e1c427706a3d1cfe8
Infant Frontender <newbornfrontender@gmail.com>
2019-07-18 18:55:25 +0300

    feat: add doiuse and postcss-reporter

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,14 @@
+const { browserslist: browsers } = require('./package.json');
+
 module.exports = ({ production }) => ({
   plugins: {
     'postcss-preset-env': {
       stage: 0,
       autoprefixer: production,
     },
+    doiuse: {
+      browsers,
+    },
+    'postcss-reporter': true,
   },
 });
